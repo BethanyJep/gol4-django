b64ed0b3caaa7ebe89154a785ee866fd632b6f48
Pikachú <engbethany4@gmail.com>
2023-06-05 19:38:02 +0300

    updates

--- helloproject/settings.py
+++ helloproject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello_world.apps.HelloworldConfig',
+    'hello_world',
 ]
 
 MIDDLEWARE = [
